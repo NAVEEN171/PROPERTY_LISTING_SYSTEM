67daef555f42eeb195268eb01b2df087bc3313fa
NAVEEN171 <naveenkumar171837@gmail.com>
2025-05-31 17:39:41 +0530

    config changed

--- config/DB.js
+++ config/DB.js
@@ -4,24 +4,33 @@ require("dotenv").config();
 let isConnected = false;
 
 const connectDB = async () => {
+  if (!process.env.MONGODB_URL) {
+    throw new Error("MONGODB_URL is not defined in the .env file");
+  }
+
   if (isConnected) {
     console.log("MongoDB already connected");
     return mongoose.connection;
   }
 
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL, {
-      bufferCommands: true,
-      serverSelectionTimeoutMS: 5000,
-      socketTimeoutMS: 45000,
-      maxPoolSize: 10,
-      minPoolSize: 2,
-    });
+    await mongoose.connect(process.env.MONGODB_URL);
     isConnected = true;
-    return conn.connection;
+    console.log("MongoDB connected");
+
+    mongoose.connection.on("disconnected", () => {
+      console.warn("MongoDB disconnected. Attempting to reconnect...");
+      isConnected = false;
+    });
+
+    mongoose.connection.on("reconnected", () => {
+      console.log("MongoDB reconnected.");
+      isConnected = true;
+    });
+
+    return mongoose.connection;
   } catch (err) {
     console.error("MongoDB connection error:", err.message);
-    throw err;
   }
 };
 
